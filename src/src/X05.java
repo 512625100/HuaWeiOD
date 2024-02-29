21abe2793bb9db9311782cc5c3783fbba898b1ea
T480 <512625100@qq.com>
2024-02-29 18:03:31 +0800

    8题

--- src/src/X05.java
+++ src/src/X05.java
@@ -10,25 +10,37 @@ public class X05 {
         String num = scanner.nextLine();
         System.out.println("请时输入URL地址：");
         ArrayList<List> objects = new ArrayList<>();
+        // 遍历日志的条数，获取url地址
         for (int i = 0; i < Integer.parseInt(num);i++){
             String urls = scanner.nextLine();
+            // 把输入的每一行地址按照/分割转换成列表中
             List<String> split = Arrays.asList(urls.split("/"));
+            // 把处理后的list放进objects列表中
             objects.add(split);
         }
         System.out.println("请输入层级和关键字：");
         String keyword = scanner.nextLine();
         List<String> list = Arrays.asList(keyword.split(" "));
+
         Integer res = 0;
+        // 遍历列表
         for (List object : objects) {
+            // 如果url转换成的列表中元素的个数小于层级的数字 退出循环
             if (object.size() - 1 < Integer.parseInt(list.get(0))){
                 break;
-            }else {
+            }
+            // 如果url转成的列表中元素的个数不小于
+            else {
+                // 获取url列表中的层级关键字
                 String s = object.get(Integer.parseInt(list.get(0))).toString();
+                // 判断获取的关键字和输入的关键字是否相等
                 if (s.equals(list.get(1))) {
+                    // 相等加1
                     res ++;
                 }
             }
         }
+        // 输出相等的个数
         System.out.println(res);
     }
 }
--- src/src/X06.java
+++ src/src/X06.java
@@ -13,18 +13,27 @@ public class X06 {
         List<String> list1 = Arrays.asList(line1.split(" "));
         List<String> list2 = Arrays.asList(line2.split(" "));
         ArrayList<Integer> sort = new ArrayList<>();
+        // 遍历第一行数组
         for(int i = 1;i < list1.size();i++){
+            // 按照顺序获取第一行数组的每个元素
             Integer num1 = Integer.parseInt(list1.get(i));
+            // 遍历第二行数组
             for (int j=1;j<list2.size();j++){
+                // 按照顺序获取第二行数组的每个元素
                 Integer num2 = Integer.parseInt(list2.get(j));
+                // 第一行数组的元素与第二行数组的元素相加
                 Integer count = num1+num2;
+                // 相加的得到的和加入到sort列表中
                 sort.add(count);
             }
         }
+        // 对sort列表按照从小到大排序
         Collections.sort(sort);
         System.out.println("后：" + sort);
+
         int z = 0;
         int m = 0;
+        // 按照输入的num（几个元素对）相加得到最小值
         for(int n = 0;n < num ;n++){
            m = sort.get(n);
            z = m + z;
