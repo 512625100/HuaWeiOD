3e8cf849d30c16265dea2e4d9cdcac618ad33105
T480 <512625100@qq.com>
2024-02-29 18:24:02 +0800

    8题

--- src/src/X03.java
+++ src/src/X03.java
@@ -8,33 +8,50 @@ public class X03 {
         System.out.println("请输入一个自然数：");
         int num = scanner.nextInt();
 
+        // 创建一个等式变量 例如 9 =
         String str = num + "=";
         StringBuilder stringBuilder = new StringBuilder();
         ArrayList<Object> objects = new ArrayList<>();
         int rs = 0;
+        // 遍历输入的自然数次数
         for (int i = 1; i <= num;i++){
+            // 创建一个变量存储自然数之和
             int z = 0;
+            // 遍历输入的自然数次数，因为第一次遍历i时，i的第一个数字是1，等第二次遍历时，i的数字为2，j就会从2开始
             for (int j = i; j <= num ; j++){
-
+                // z与j累计相加
                 z += j;
+                // 当自然数之和大于0时
                 if (z > 0){
+                    // 字符串追加，列入 1 + 2 + 3 +
                     stringBuilder.append(j + "+");
+                    // 当自然数与累计后的数字之和相等
                     if (z == num){
+                        // 计算存在结果的个数
                         rs ++;
+                        // 结果拼接，例如 9 = 2 + 3 + 4 +
                         String x = str + stringBuilder;
+                        // 把结果存入到列表中
                         objects.add(x);
                     }
+                    // 如果自然数之和大于了输入的自然数
                     if (z > num){
+                        // 清楚字符串拼接中的值
                         stringBuilder.setLength(0);
+                        // 退出循环
                         break;
                     }
                 }
             }
         }
+        // 对列表中存入的元素逆序排序
         Collections.reverse(objects);
+        // 循环遍历输出结果
         for (Object object : objects) {
+            // 字符串截取 例如 9 = 2 + 3 + 4 + 去除掉数字4后面的加号
             System.out.println(object.toString().substring(0,object.toString().length()-1));
         }
+        // 输出存在结果的个数
         System.out.println("result:" + rs);
     }
 }
--- src/src/X04.java
+++ src/src/X04.java
@@ -16,27 +16,37 @@ public class X04 {
 
         Map<String, Integer> map = new HashMap<>();
 
+        // 遍历开源数目的个数
         for (int i = 0 ; i < Integer.parseInt(num); i ++){
             int count = 0;
+            // 把纬度值列表按照空格分隔，转换成列表
             projectNum = Arrays.asList(scanner.nextLine().split(" "));
+            // 遍历开源项目权重列表，从第二个值开始
             for (int j = 1;j <= 5; j ++){
-               count += Integer.parseInt(weightNum.get(j-1)) * Integer.parseInt(projectNum.get(j));
+                // 计算每个权重值与每个纬度值的计算并相加
+                count += Integer.parseInt(weightNum.get(j-1)) * Integer.parseInt(projectNum.get(j));
             }
+            // 把每个开源项目和热度存入字典
             map.put(projectNum.get(0),count);
         }
         List<Map.Entry<String, Integer>> sortedList = new ArrayList<>(map.entrySet());
+        // 对字典进行排序
         Collections.sort(sortedList, new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                // 如果字典中的两个value值相等
                 if (o1.getValue().equals(o2.getValue())) {
+                    // 按照key的小写字母的字典序排序
 //                    return o2.getKey().compareToIgnoreCase(o1.getKey());
                     return o1.getKey().toLowerCase().compareTo(o2.getKey().toLowerCase());
                 } else {
+                    // 否则按照value值大小排序
                     return o2.getValue().compareTo(o1.getValue());
                 }
             }
         });
 
+        // 遍历打印字典中的key
         for (Map.Entry<String, Integer> entry : sortedList) {
             System.out.println(entry.getKey());
         }
