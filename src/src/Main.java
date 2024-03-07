626b4f2fcec4a3cf0351bac8d73f002b4caca2df
T480 <512625100@qq.com>
2024-03-07 18:31:04 +0800

    18题

--- src/src/Main.java
+++ src/src/Main.java
@@ -9,51 +9,38 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int number = scanner.nextInt();
-        int N = scanner.nextInt();
-        int X = scanner.nextInt();
-
-        int[] return_list = new int[number];
-        int[] risk_list = new int[number];
-        int[] max_cost_list = new int[number];
-
-        for (int i = 0; i < number; i++) {
-            return_list[i] = scanner.nextInt();
+        int wordsCount = scanner.nextInt();
+        String[] wordArr = new String[wordsCount];
+        for (int i = 0; i < wordsCount; i++) {
+            wordArr[i] = scanner.next();
         }
-
-        for (int i = 0; i < number; i++) {
-            risk_list[i] = scanner.nextInt();
-        }
-
-        for (int i = 0; i < number; i++) {
-            max_cost_list[i] = scanner.nextInt();
-        }
-
-        int[] max_status = new int[number];
-        int max_return = 0;
-        int max_return_risk = 0;
-
-        for (int i = 0; i < number; i++) {
-            for (int j = i + 1; j < number; j++) {
-                if (risk_list[i] + risk_list[j] <= X) {
-                    int max_return_product_index = return_list[i] > return_list[j] ? i : j;
-                    int other_return_product_index = i + j - max_return_product_index;
-                    int max_return_cost = Math.min(N, max_cost_list[max_return_product_index]);
-                    int other_return_cost = Math.min(N - max_return_cost, max_cost_list[other_return_product_index]);
-                    int cur_return = return_list[max_return_product_index] * max_return_cost + return_list[other_return_product_index] * other_return_cost;
-                    if (cur_return > max_return) {
-                        max_return = cur_return;
-                        max_return_risk = risk_list[i] + risk_list[j];
-                        max_status[max_return_product_index] = max_return_cost;
-                        max_status[other_return_product_index] = other_return_cost;
-                    }
-                }
+        String chars = scanner.next();
+        char[] charArr = new char[26];
+        int anyNum = 0;
+        int res = 0;
+        for (char c : chars.toCharArray()) {
+            if (c == '?') {
+                anyNum++;
+            } else {
+                charArr[c - 'a']++;
             }
         }
-
-        for (int i = 0; i < number; i++) {
-            System.out.print(max_status[i] + " ");
+        for (String word : wordArr) {
+            char[] wordsplit = new char[26];
+            for (char c : word.toCharArray()) {
+                wordsplit[c - 'a']++;
+            }
+            int needAnyNum = 0;
+            for (int i = 0; i < 26; i++) {
+                int i1 = wordsplit[i] - charArr[i];
+                if (i1 > 0) {
+                    needAnyNum += i1;
+                }
+            }
+            if (needAnyNum <= anyNum) {
+                res++;
+            }
         }
+        System.out.println(res);
     }
 }
